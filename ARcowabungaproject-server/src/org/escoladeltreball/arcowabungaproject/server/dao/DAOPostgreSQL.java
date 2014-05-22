38a2b8b93f6ad415b8ca82f036089a6b194b50e3
Joaquim Dalmau Torvà <jdalmaut@gmail.com>
2014-05-22 17:24:43 +0200

    Add updateDrinkById method in DAOPostgreSQL.java

--- ARcowabungaproject-server/src/org/escoladeltreball/arcowabungaproject/server/dao/DAOPostgreSQL.java
+++ ARcowabungaproject-server/src/org/escoladeltreball/arcowabungaproject/server/dao/DAOPostgreSQL.java
@@ -217,7 +217,9 @@ public class DAOPostgreSQL extends DAOFactory {
      * Update Ingredient row by id
      * 
      * @param id
-     *            of row to delete
+     *            of row to update
+     * @param set
+     *            the string contains the new values
      */
     public void updateIngredientById(String id, String set) {
 	Connection con = null;
@@ -228,6 +230,44 @@ public class DAOPostgreSQL extends DAOFactory {
 	    stm.executeUpdate("UPDATE " + DAOFactory.TABLE_INGREDIENT + " SET "
 		    + set + " WHERE " + DAOFactory.COLUMNS_NAME_INGREDIENT[0]
 		    + "=" + id + ";");
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} finally {
+	    if (stm != null) {
+		try {
+		    stm.close();
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		}
+	    }
+	    if (con != null) {
+		try {
+		    con.close();
+		} catch (SQLException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}
+    }
+
+    /**
+     * Update Drink row by id
+     * 
+     * @param id
+     *            of row to update
+     * @param set
+     *            the string contains the new values
+     */
+    public void updateDrinksById(String id, String set) {
+	Connection con = null;
+	Statement stm = null;
+	try {
+	    con = connectToDatabase();
+	    stm = con.createStatement();
+	    stm.executeUpdate("UPDATE " + DAOFactory.TABLE_DRINKS + " SET "
+		    + set + " WHERE " + DAOFactory.COLUMNS_NAME_DRINKS[0] + "="
+		    + id + ";");
 	} catch (SQLException e) {
 	    e.printStackTrace();
 	    throw new RuntimeException(e);

b48036e7388a41c0380f982834227f8ce93539fb
Victor Purcallas <vpurcallas@gmail.com>
2014-05-23 10:33:32 +0200

    Comment and clean PizzaWorld class

--- ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/ar/PizzaWorld.java
+++ ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/ar/PizzaWorld.java
@@ -3,24 +3,26 @@
  *  
  *  This file is part of ARcowabungaproject.
  *  
- *  Copyright 2014 	Bernabe Gonzalez Garcia <bernagonzga@gmail.com>
- *  			Marc Sabate Piñol <masapim@hotmail.com>
- *  			Victor Purcallas Marchesi <vpurcallas@gmail.com>
- *  			Joaquim Dalmau Torva <jdalmaut@gmail.com>
+ *  Bernabe Gonzalez Garcia <bernagonzga@gmail.com>
+ *  Joaquim Dalmau Torva <jdalmaut@gmail.com>
+ *  Marc Sabate Piñol <masapim@hotmail.com>
+ *  Victor Purcallas Marchesi <vpurcallas@gmail.com>
+ *  			
  *
- *   ARcowabungaproject is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ *  ARcowabungaproject is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- *   ARcowabungaproject is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ *  ARcowabungaproject is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>. 
+ *  You should have received a copy of the GNU General Public License
+ *  along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>. 
  */
+
 package org.escoladeltreball.arcowabungaproject.ar;
 
 import gl.GLCamera;
@@ -31,28 +33,18 @@ import javax.microedition.khronos.opengles.GL10;
 import util.Vec;
 import worldData.World;
 
-/**
- * @author local
- * 
- */
 public class PizzaWorld extends World {
 
-    /**
-     * think of this as the position on the screen
-     */
+    // position on the screen
     private Vec myScreenPosition;
-
-    /**
-     * the camera which is responsible to display the world correctly
-     */
+    // camera which is responsible to display the world correctly
     private GLCamera myCamera;
-
-    /**
-     * think of this as the scale of the whole world on the screen
-     */
+    // scale of the whole world on the screen
     private Vec myScale;
 
     /**
+     * Sets the camera in world
+     * 
      * @param glCamera
      */
     public PizzaWorld(GLCamera glCamera) {
@@ -84,12 +76,22 @@ public class PizzaWorld extends World {
     // PRIVATE METHODS
     // ====================
 
+    /**
+     * Load the screen position
+     * 
+     * @param gl
+     */
     private void glLoadScreenPosition(GL10 gl) {
 	if (myScreenPosition != null)
 	    gl.glTranslatef(myScreenPosition.x, myScreenPosition.y,
 		    myScreenPosition.z);
     }
 
+    /**
+     * Load the world to screen
+     * 
+     * @param gl
+     */
     private void glLoadScale(GL10 gl) {
 	if (myScale != null)
 	    gl.glScalef(myScale.x, myScale.y, myScale.z);
@@ -99,6 +101,10 @@ public class PizzaWorld extends World {
     // OVERRIDE METHODS
     // ====================
 
+    /**
+     * Load the screen position, make the camera, set the camera scale and draw
+     * the GL10 objects
+     */
     @Override
     public void render(GL10 gl, Renderable parent) {
 	glLoadScreenPosition(gl);

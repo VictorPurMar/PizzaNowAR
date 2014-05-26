dc3a8a21afbddc0c32e401e5a11c9d7a4c6b723f
Bernabe Gonzalez <bernagonzga@gmail.com>
2014-05-26 19:35:49 +0200

    Fix layout from makeyourpizza, and apply custom pizza option on activity

--- ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/activities/MakeYourOwnActivity.java
+++ ARcowabungaproject/src/org/escoladeltreball/arcowabungaproject/activities/MakeYourOwnActivity.java
@@ -23,11 +23,14 @@
  */
 package org.escoladeltreball.arcowabungaproject.activities;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import org.escoladeltreball.arcowabungaproject.R;
 import org.escoladeltreball.arcowabungaproject.adapters.IngredientSetAdapter;
 import org.escoladeltreball.arcowabungaproject.model.Ingredient;
+import org.escoladeltreball.arcowabungaproject.model.Pizza;
+import org.escoladeltreball.arcowabungaproject.model.ShoppingCart;
 import org.escoladeltreball.arcowabungaproject.model.system.Pizzeria;
 import org.escoladeltreball.arcowabungaproject.utils.CustomTextView;
 
@@ -37,9 +40,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MakeYourOwnActivity extends Activity implements OnClickListener {
 
@@ -97,6 +104,10 @@ public class MakeYourOwnActivity extends Activity implements OnClickListener {
 	CustomTextView.customTextView(this, tv);
 	tv = (TextView) findViewById(R.id.textTypePizza);
 	CustomTextView.customTextView(this, tv);
+	tv = (TextView) findViewById(R.id.button_menu_text);
+	CustomTextView.customTextView(this, tv);
+	tv = (TextView) findViewById(R.id.button_cart_text);
+	CustomTextView.customTextView(this, tv);
 
 	RadioButton rb = (RadioButton) findViewById(R.id.radioDoughFine);
 	CustomTextView.customTextView(this, rb);
@@ -104,11 +115,65 @@ public class MakeYourOwnActivity extends Activity implements OnClickListener {
 	CustomTextView.customTextView(this, rb);
 	rb = (RadioButton) findViewById(R.id.radioDoughBig);
 	CustomTextView.customTextView(this, rb);
+
+	// Add listeners
+	LinearLayout ly = (LinearLayout) findViewById(R.id.bottom_menu);
+	ly.setOnClickListener(this);
+	ly = (LinearLayout) findViewById(R.id.button_cart);
+	ly.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-	finish();
+	if (v.getId() == R.id.button_menu) {
+	    finish();
+	} else if (v.getId() == R.id.button_cart) {
+	    ListView lv = (ListView) findViewById(R.id.listView);
+
+	    ShoppingCart sc = Pizzeria.getInstance().getShoppingCart();
+	    Pizzeria p = Pizzeria.getInstance();
+	    RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+	    int rbId = rg.getCheckedRadioButtonId();
+	    String pizzaValue = null;
+	    if (rbId == R.id.radioDoughFine) {
+		pizzaValue = Pizza.MASSTYPE_THIN;
+	    } else if (rbId == R.id.radioDoughNormal) {
+		pizzaValue = Pizza.MASSTYPE_NORMAL;
+	    } else if (rbId == R.id.radioDoughBig) {
+		pizzaValue = Pizza.MASSTYPE_THICK;
+	    }
+	    TextView tvNamePizza = (TextView) findViewById(R.id.nameCustomPizza);
+	    int nextId = sc.getNextCustomId();
+	    Pizza pizza = new Pizza(nextId, tvNamePizza.getText().toString(),
+		    11, 150, 0, pizzaValue, Pizza.TYPE_CUSTOM_SAVED,
+		    Pizza.SIZE_SMALL);
+
+	    int childCount = lv.getChildCount();
+	    for (int i = 0; i < childCount; i++) {
+		View view = lv.getChildAt(i);
+		ToggleButton tb = (ToggleButton) view
+			.findViewById(R.id.toggle_button_ingredient_item);
+		if (tb.isChecked()) {
+		    String name = tb.getText().toString();
+
+		    Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+		    Set<Ingredient> ingredients = Pizzeria.getInstance()
+			    .getIngredients();
+		    Iterator it = ingredients.iterator();
+		    while (it.hasNext()) {
+			Ingredient tempIng = (Ingredient) it.next();
+			Toast.makeText(this, tempIng.getName(),
+				Toast.LENGTH_LONG).show();
+			if (tempIng.getName().equals(name)) {
+			    pizza.addIngredient(tempIng, 1);
+			}
+		    }
+		}
+	    }
+	    p.addCustomSavedPizza(pizza);
+
+	    finish();
+	}
     }
 
     @Override
